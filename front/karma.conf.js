dd635b1e268912e0ee94afff016731c701486a83
Strackz <strackzdesign@gmail.com>
2024-10-11 15:50:19 +0200

    feat: npx codecov

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -1,35 +1,10 @@
 module.exports = function (config) {
   config.set({
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-
-    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
-
-    preprocessors: {
-      'src/**/*.ts': ['coverage']
-    },
-
-    coverageReporter: {
-      type: 'lcov',
-      dir: 'coverage/'
-    },
-
-    webpack: {
-      module: {
-        rules: [
-          {
-            test: /\.ts$/,
-            use: {
-              loader: 'istanbul-instrumenter-loader',
-              options: { esModules: true }
-            },
-            enforce: 'post',
-            exclude: /node_modules|\.spec\.ts$/
-          }
-        ]
+    browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+    ChromeHeadlessNoSandbox: {
+      base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
       }
     }
   });
